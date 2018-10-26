0d7eee0c419961b55178ce43003cca99a20e6919
Sukhbeer Singh <sukhbeer940@gmail.com>
2018-10-26 23:19:51 +0530

    Creating Stack Using LinkedList  in Java

---
+++ Java/StackUsingLinkedList.java
@@ -0,0 +1,49 @@
+class Node<T> {
+	T data;
+	Node<T> next;
+
+	Node(T data) {
+		this.data = data;
+	}
+}
+
+public class StackUsingLinkedList<T> {
+
+	private Node<T> top;
+	private int size;
+
+	public StackUsingLinkedList() {
+		top = null;
+		size = 0;
+	}
+
+	public void push(T data) {
+		Node<T> nyaNode = new Node<>(data);
+		size++;
+		if (top == null) {
+			top = nyaNode;
+			return;
+		}
+		nyaNode.next = top;
+		top = nyaNode;
+	}
+
+	public T pop() throws StackUnderFlowException {
+		if (top == null) {
+			throw new StackUnderFlowException();
+		}
+		size--;
+		Node<T> temp = top;
+		top = top.next;
+		return temp.data;
+	}
+
+	public int size() {
+		return size;
+	}
+
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+}
